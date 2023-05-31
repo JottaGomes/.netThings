022ef71224d8fa4529b893f050e8dd4e1893da80
joaofegomes <joaofegomes@deloitte.pt>
2023-05-31 11:11:42 +0100

    "adding delete and PUt methods"

---
+++ Dtos/Character/GetCharacterDTO.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dtos.Character
+{
+    public class GetCharacterDTO
+    {
+        
+    public int id { get; set; }
+    public String Name { get; set; } = "Sangoku"; 
+    public int hitPoints { get; set; } = 100; 
+    public int streng { get; set; } = 10; 
+    public int defence { get; set; } = 10; 
+    public int intelligence { get; set; } = 10; 
+    public rpgClass Class { get; set; } = rpgClass.Knight;  
+    }
+}
