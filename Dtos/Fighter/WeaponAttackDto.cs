7b0c24a3e17069a1c10c2613c00a8d586c1736e0
joaofegomes <joaofegomes@deloitte.pt>
2023-06-27 16:37:37 +0100

    fighting impl

---
+++ Dtos/Fighter/WeaponAttackDto.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dtos.Fighter
+{
+    public class WeaponAttackDto
+    {
+        public int AttackerId { get; set; }
+
+        public int OponentId { get; set; }
+
+        
+    }
+}
